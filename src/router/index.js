b387ad78ecc39311f36461bca9793956be04ca4a
Kai <kaikai806148689@qq.com>
2020-02-18 15:14:21 +0800

    wsk commit
    整理工程结构

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ import componentsRouter from './modules/components'
 import chartsRouter from './modules/charts'
 import tableRouter from './modules/table'
 import nestedRouter from './modules/nested'
-import energyRouter from './modules/energy'
+import energyRouter from './project/energy/energy'
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
