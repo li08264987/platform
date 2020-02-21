58e11e2d7ae0aba5d8ff2640ad4f77a6ce874606
li08264987 <245183526@qq.com>
2020-02-21 14:56:41 +0800

    Update reportTable.js

--- src/router/project/reportTable/reportTable.js
+++ src/router/project/reportTable/reportTable.js
@@ -1,8 +1,14 @@
 import Layout from '@/layout'
 
+<<<<<<< Updated upstream
 const reportTableRouters = [{
   path: '/reportTable',
   redirect: '/reportTable/kongYa', // 重定向地址，在面包屑中点击会重定向去的地址
+=======
+const reportTableRouter = [{
+  path: '/reportTable',
+  redirect: '/reportTable/kongya', // 重定向地址，在面包屑中点击会重定向去的地址
+>>>>>>> Stashed changes
   component: Layout,
   name: 'reportTable',
   meta: {
@@ -12,9 +18,15 @@ const reportTableRouters = [{
   },
   children: [
     {
+<<<<<<< Updated upstream
       path: 'kongYa',
       component: () => import('@/views/project/reportTable/kongYa'),
       name: 'reportTablekongYa',
+=======
+      path: 'kongya',
+      component: () => import('@/views/reportTable/kongya/index'),
+      name: 'reportTableKongya',
+>>>>>>> Stashed changes
       meta: {
         title: '空压系统',
         roles: ['admin'],
@@ -23,9 +35,15 @@ const reportTableRouters = [{
       }
     },
     {
+<<<<<<< Updated upstream
       path: 'lengShui',
       component: () => import('@/views/project/reportTable/kongYa'),
       name: 'reportTablekongYa',
+=======
+      path: 'kongya',
+      component: () => import('@/views/reportTable/kongya/index'),
+      name: 'reportTableKongya',
+>>>>>>> Stashed changes
       meta: {
         title: '冷水系统',
         roles: ['admin'],
@@ -34,7 +52,11 @@ const reportTableRouters = [{
       }
     },
     {
+<<<<<<< Updated upstream
       path: 'danQi',
+=======
+      path: 'qingdan',
+>>>>>>> Stashed changes
       component: () => import('@/views/icons/index'),
       name: 'reportTableQingdan',
       meta: {
@@ -43,8 +65,13 @@ const reportTableRouters = [{
         icon: 'documentation'
       }
     }, {
+<<<<<<< Updated upstream
       path: 'qingQi',
       component: () => import('@/views/project/reportTable/kongYa'),
+=======
+      path: 'zhenkong',
+      component: () => import('@/views/reportTable/qingdan'),
+>>>>>>> Stashed changes
       name: 'reportTableZhenkong',
       meta: {
         title: '氢气系统',
@@ -53,7 +80,11 @@ const reportTableRouters = [{
       }
     }, {
       path: 'dianli',
+<<<<<<< Updated upstream
       component: () => import('@/views/project/reportTable/kongYa'),
+=======
+      component: () => import('@/views/reportTable/qingdan'),
+>>>>>>> Stashed changes
       name: 'reportTableDianli',
       meta: {
         title: '电力系统',
@@ -61,7 +92,11 @@ const reportTableRouters = [{
       }
     }, {
       path: 'lengshui',
+<<<<<<< Updated upstream
       component: () => import('@/views/project/reportTable/kongYa'),
+=======
+      component: () => import('@/views/reportTable/qingdan'),
+>>>>>>> Stashed changes
       name: 'reportTableLengshui',
       meta: {
         title: '供电系统',
@@ -69,7 +104,11 @@ const reportTableRouters = [{
       }
     }, {
       path: 'reshui',
+<<<<<<< Updated upstream
       component: () => import('@/views/project/reportTable/kongYa'),
+=======
+      component: () => import('@/views/reportTable/qingdan'),
+>>>>>>> Stashed changes
       name: 'reportTableReshui',
       meta: {
         title: '热水系统',
@@ -89,7 +128,11 @@ const reportTableRouters = [{
   children: [
     {
       path: 'first',
+<<<<<<< Updated upstream
       component: () => import('@/views/project/reportTable/kongYa'),
+=======
+      component: () => import('@/views/reportTable/first'),
+>>>>>>> Stashed changes
       name: 'reportTableFirst',
       meta: {
         title: '一号车间',
@@ -97,7 +140,11 @@ const reportTableRouters = [{
       }
     }, {
       path: 'second',
+<<<<<<< Updated upstream
       component: () => import('@/views/project/reportTable/kongYa'),
+=======
+      component: () => import('@/views/reportTable/second'),
+>>>>>>> Stashed changes
       name: 'reportTableSecond',
       meta: {
         title: '二号车间',
@@ -105,31 +152,50 @@ const reportTableRouters = [{
       }
     }, {
       path: 'third',
+<<<<<<< Updated upstream
       component: () => import('@/views/project/reportTable/kongYa'),
+=======
+      component: () => import('@/views/reportTable/third'),
+>>>>>>> Stashed changes
       name: 'reportTableThird',
       meta: {
         title: '三号车间',
         icon: 'documentation'
       }
     }, {
+<<<<<<< Updated upstream
       path: 'fouth',
       component: () => import('@/views/project/reportTable/kongYa'),
+=======
+      path: 'fourth',
+      component: () => import('@/views/reportTable/fourth'),
+>>>>>>> Stashed changes
       name: 'reportTableFouth',
       meta: {
         title: '四号车间',
         icon: 'documentation'
       }
     }, {
+<<<<<<< Updated upstream
       path: 'jiaoshui',
       component: () => import('@/views/project/reportTable/kongYa'),
+=======
+      path: 'jiaoShui',
+      component: () => import('@/views/reportTable/jiaoShui'),
+>>>>>>> Stashed changes
       name: 'reportTableJiaoshui',
       meta: {
         title: '胶水厂区',
         icon: 'documentation'
       }
     }, {
+<<<<<<< Updated upstream
       path: 'dongli',
       component: () => import('@/views/project/reportTable/kongYa'),
+=======
+      path: 'dongLi',
+      component: () => import('@/views/reportTable/dongLi'),
+>>>>>>> Stashed changes
       name: 'reportTableDongli',
       meta: {
         title: '动力车间',
@@ -139,4 +205,8 @@ const reportTableRouters = [{
   ]
 }]
 
+<<<<<<< Updated upstream
 export default reportTableRouters
+=======
+export default reportTableRouter
+>>>>>>> Stashed changes
