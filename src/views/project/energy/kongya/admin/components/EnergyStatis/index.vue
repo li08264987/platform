cec567461cf81c053e2ebf597afe7ab9d0c2a536
Kai <kaikai806148689@qq.com>
2020-04-06 11:40:59 +0800

    wsk
    1

--- src/views/project/energy/kongya/admin/components/EnergyStatis/index.vue
+++ src/views/project/energy/kongya/admin/components/EnergyStatis/index.vue
@@ -5,7 +5,27 @@
         <el-col :span="16" class="dash-boeder-el-col">
           <div class="left-container">
             <div class="left-container-top">
-              <div class="progress-charts-container" />
+              <div class="progress-charts-container">
+                <div class="progress-chart-container">
+                  <div class="progress-chart-title"><span>能耗进度</span></div>
+                  <div class="progress-chart-bar">
+                    <el-progress
+                      :percentage="80"
+                      :stroke-width="15"
+                      :show-text="false"
+                      class="energy-statis-nenghao-pragress nenghao"
+                    />
+                  </div>
+                </div>
+                <div class="progress-chart-container">
+                  <div class="progress-chart-title"><span>能效进度</span></div>
+                  <div class="progress-chart-bar" />
+                </div>
+                <div class="progress-chart-container">
+                  <div class="progress-chart-title"><span>能费进度</span></div>
+                  <div class="progress-chart-bar" />
+                </div>
+              </div>
               <div class="percent-chart-container" />
             </div>
             <div class="left-container-bottom" />
@@ -51,13 +71,14 @@ export default {
     border-bottom: solid 1px #E4E9F0;
     flex-grow: 0;
     flex-shrink: 0;
+    display: flex;
+    flex-direction: row;
   }
   .left-container-bottom {
     height: 55%;
     width: 100%;
     flex-grow: 0;
     flex-shrink: 0;
-    display: flex;
   }
   .progress-charts-container {
     width: 55%;
@@ -65,11 +86,41 @@ export default {
     border-right: solid 1px #E4E9F0;
     flex-grow: 0;
     flex-shrink: 0;
+    display: flex;
+    flex-direction: column;
+  }
+  .progress-chart-container {
+    width: 100%;
+    height: calc(100% / 3);
+    border-bottom: solid 1px #E4E9F0;
+    display: flex;
+    flex-direction: column;
+    padding: 15px 30px;
+  }
+  .progress-chart-container:last-child {
+    border-bottom: none;
   }
   .percent-chart-container {
     width: 45%;
     height: 100%;
     flex-grow: 0;
     flex-shrink: 0;
+  }
+  .progress-chart-title {
+    font-size: 16px;
+    color: rgba(51,51,51,1);
+    font-weight: bold;
+    margin-top: 10px;
+    padding: 0px 10px;
+    display: flex;
+    border-left: solid 2px rgba(74,98,245,1);
+  }
+</style>
+<style lang="scss">
+  .energy-statis-nenghao-pragress.nenghao .el-progress-bar__inner {
+    background-image: linear-gradient(90deg,rgba(255,161,146,1) 0%,rgba(240,114,94,1) 100%);
+  }
+  .energy-statis-nenghao-pragress .el-progress-bar__outer {
+    background-color: rgba(234,234,234,1);
   }
 </style>
