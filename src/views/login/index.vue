a1b2c8d2e34cc0562d6f1ec2f9d513fd032b0709
li08264987 <36978244+li08264987@users.noreply.github.com>
2020-04-16 11:14:13 +0800

    Update index.vue

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -89,7 +89,7 @@ export default {
     } */
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+        callback(new Error('密码不能少于6位字符'))
       } else {
         callback()
       }
